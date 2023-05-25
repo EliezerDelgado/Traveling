518ccda7499b93a9497fb5735566db35cb7d474a
eliezer <delgadoeliezer0@gmail.com>
2023-05-25 16:15:31 +0200

    Terminar Login

---
+++ app/src/main/java/com/travel_world/traveling/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.travel_world.traveling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.travel_world.traveling.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding loginBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(loginBinding.getRoot());
+        setBindRepo();
+    }
+
+    private void setBindRepo() {
+    }
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.button_login_forgot:
+                Snackbar.make(view,getString(R.string.button_login_forgot_onclick),Snackbar.LENGTH_LONG).show();
+                break;
+            case R.id.button_login_register:
+                Snackbar.make(view,getString(R.string.button_login_register_onclick),Snackbar.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
