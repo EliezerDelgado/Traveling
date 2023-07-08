158ea52d9f07b85fcec99aba16170ac087150902
eliezer <delgadoeliezer0@gmail.com>
2023-07-08 20:22:18 +0200

    Empezando la practica 4

--- app/src/main/java/com/travel_world/traveling/AgeRange.java
+++ app/src/main/java/com/travel_world/traveling/AgeRange.java
@@ -1,6 +1,5 @@
 package com.travel_world.traveling;
-
-public abstract class AgeRange {
+public abstract class AgeRange{
     public final static String BABY = "0-5";
     public final static String CHILD = "6-11";
     public final static String TEENAGER = "12-17";
--- app/src/main/java/com/travel_world/traveling/SignUpActivity.java
+++ app/src/main/java/com/travel_world/traveling/SignUpActivity.java
@@ -1,14 +1,33 @@
 package com.travel_world.traveling;
 
+import static com.travel_world.traveling.AgeRange.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
+
+import com.travel_world.traveling.databinding.ActivitySignUpBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private ActivitySignUpBinding binding;
+    private ArrayAdapter adapterSpinnerAges;
+    private  ArrayList<String> arrayAges;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        arrayAges = new ArrayList<>(Arrays.asList(BABY,CHILD,TEENAGER,ADULT));
+        setValueSpinnerAges();
+    }
+    private void setValueSpinnerAges() {
+        adapterSpinnerAges = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, arrayAges);
+        binding.agesRangeSignup.setAdapter(adapterSpinnerAges);
     }
 }
