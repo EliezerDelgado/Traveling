3aa2f1c22c00260d70baf1e491246325f71c23b2
eliezer <delgadoeliezer0@gmail.com>
2023-07-08 19:14:23 +0200

    Empezando la practica 4

---
+++ app/src/main/java/com/travel_world/traveling/AgeRange.java
@@ -0,0 +1,8 @@
+package com.travel_world.traveling;
+
+public abstract class AgeRange {
+    public final static String BABY = "0-5";
+    public final static String CHILD = "6-11";
+    public final static String TEENAGER = "12-17";
+    public final static String ADULT = "18-99";
+}
--- app/src/main/java/com/travel_world/traveling/User.java
+++ app/src/main/java/com/travel_world/traveling/User.java
@@ -2,15 +2,21 @@ package com.travel_world.traveling;
 
 public class User {
     private String name;
+    private String lastName;
     private String password;
+
+    private String ageRange;
 
     public User() {
         name = "";
+        lastName = "";
         password = "";
     }
-    public User(String name, String password) {
+    public User(String name, String lastName, String password, String ageRange) {
         this.name = name;
+        this.lastName = lastName;
         this.password = password;
+        this.ageRange = ageRange;
     }
 
     public String getName() {
@@ -29,4 +35,19 @@ public class User {
         this.password = password;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAgeRange() {
+        return ageRange;
+    }
+
+    public void setAgeRange(String ageRange) {
+        this.ageRange = ageRange;
+    }
 }
