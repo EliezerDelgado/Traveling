15d249446f87ae93bfa3d80187a8005c04aafa27
eliezer <delgadoeliezer0@gmail.com>
2023-07-08 22:02:09 +0200

    Layout terminado de la practica 4

--- app/src/main/java/com/travel_world/traveling/SignUpActivity.java
+++ app/src/main/java/com/travel_world/traveling/SignUpActivity.java
@@ -3,7 +3,9 @@ package com.travel_world.traveling;
 import static com.travel_world.traveling.AgeRange.*;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
@@ -21,11 +23,20 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setBarColor();
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         arrayAges = new ArrayList<>(Arrays.asList(BABY,CHILD,TEENAGER,ADULT));
         setValueSpinnerAges();
     }
+
+    private void setBarColor() {
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(
+                getResources().getColor(R.color.teal_700,getTheme()))
+        );
+        getWindow().setStatusBarColor(getResources().getColor(R.color.teal_900,getTheme()));
+    }
+
     private void setValueSpinnerAges() {
         adapterSpinnerAges = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, arrayAges);
         binding.agesRangeListSignup.setAdapter(adapterSpinnerAges);
