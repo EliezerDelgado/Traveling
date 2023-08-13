544503d70d0e1779c469a552a8e252991b1a2f8b
eliezer <delgadoeliezer05@gmail.com>
2023-08-13 17:01:21 +0200

    enpezando actividad 6

--- app/src/main/java/com/travel_world/traveling/ui/LoginFragment.java
+++ app/src/main/java/com/travel_world/traveling/ui/LoginFragment.java
@@ -1,66 +1,142 @@
 package com.travel_world.traveling.ui;
 
+import static com.travel_world.traveling.data.constants.Keys.KEY_USER;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.travel_world.traveling.R;
+import com.travel_world.traveling.data.model.User;
+import com.travel_world.traveling.databinding.FragmentLoginBinding;
+import com.travel_world.traveling.utils.AlertDialogs;
+import com.travel_world.traveling.utils.Intents;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
+    private User user;
+    private FragmentLoginBinding binding;
+    private ActivityResultLauncher<Intent> resultRegister = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if( result.getResultCode() == Activity.RESULT_OK)
+                {
+                    this.user = result.getData().getExtras().getParcelable(KEY_USER);
+                }
+            });
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        binding = FragmentLoginBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        user = new User();
+        buttonListener();
+        inputListener();
+    }
+
+    private void buttonListener() {
+        binding.buttonLoginRegister.setOnClickListener(v ->
+                resultRegister.launch(Intents.intentActivity(requireContext(), RegisterFragment.class))
+        );
+        binding.buttonLogin.setOnClickListener(v->
+                startActivityToHomeActivity()
+        );
+    }
+
+    private void startActivityToHomeActivity() {
+        if(binding.nameTextLogin.getText().toString().equals(user.getName())
+                && binding.passwordTextLogin.getText().toString().equals(user.getPassword())) {
+            Bundle bundle = new Bundle();
+            bundle.putParcelable(KEY_USER, this.user);
+            startActivity(Intents.intentActivityWithExtras(requireContext(), HomeActivity.class, bundle));
+        }
+        else
+            showErrorLoginMessage();
+
+    }
+
+    private void showErrorLoginMessage()
+    {
+        AlertDialogs.createSimpleInformativeDialog(requireActivity(),
+                getResources().getString(R.string.error_login_incorrect_login_or_password),
+                getResources().getString(R.string.confirm_message_login)
+        ).show();
+    }
+
+    private void inputListener() {
+        binding.nameTextLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.buttonLogin.setEnabled(
+                        !s.toString().isEmpty()
+                                && !binding.passwordTextLogin.getText().toString().isEmpty()
+                );
+            }
+        });
+
+        binding.passwordTextLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.buttonLogin.setEnabled(
+                        !s.toString().isEmpty()
+                                && !binding.nameTextLogin.getText().toString().isEmpty()
+                );
+            }
+        });
+    }
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.button_login_forgot:
+                Snackbar.make(view,getString(R.string.button_login_forgot_onclick),Snackbar.LENGTH_LONG).show();
+                break;
+            case R.id.button_login_register:
+                Snackbar.make(view,getString(R.string.button_login_register_onclick),Snackbar.LENGTH_LONG).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/travel_world/traveling/ui/RegisterFragment.java
+++ app/src/main/java/com/travel_world/traveling/ui/RegisterFragment.java
@@ -3,21 +3,25 @@ import static com.travel_world.traveling.data.constants.Keys.KEY_USER;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.travel_world.traveling.R;
 import com.travel_world.traveling.data.constants.UserRegex;
 import com.travel_world.traveling.data.model.User;
-import com.travel_world.traveling.databinding.ActivityRegisterBinding;
+import com.travel_world.traveling.databinding.FragmentRegisterBinding;
 import com.travel_world.traveling.utils.Intents;
 import com.travel_world.traveling.utils.AskPermissions;
 import com.travel_world.traveling.utils.UtilsStrings;
@@ -25,34 +29,40 @@ import com.travel_world.traveling.utils.UtilsStrings;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class RegisterActivity extends AppCompatActivity {
-    private ActivityRegisterBinding registerBinding;
+public class RegisterFragment extends Fragment {
+    private FragmentRegisterBinding binding;
     private ArrayAdapter adapterSpinnerAges;
     private  ArrayList<String> arrayAges;
 
     private ActivityResultLauncher<Intent> resultCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             result -> {
-                registerBinding.buttonPhotoRegister.setImageURI(Intents.getCameraImagenReturn());
+                binding.buttonPhotoRegister.setImageURI(Intents.getCameraImagenReturn());
             });
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        registerBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
-        setContentView(registerBinding.getRoot());
         arrayAges = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.age_range)));
         setValueSpinnerAges();
         buttonListener();
         inputListener();
     }
+
+    @Nullable
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentRegisterBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
     private void buttonListener() {
-        registerBinding.buttonPhotoRegister.setOnClickListener(v -> {
+        binding.buttonPhotoRegister.setOnClickListener(v -> {
             launchCamera();
 
         });
-        registerBinding.buttonPrivacyRegister.setOnClickListener(v->
+        binding.buttonPrivacyRegister.setOnClickListener(v->
                 startActivity(Intents.openPage(getString(R.string.web_developer_google)))
         );
-        registerBinding.buttonConfirmRegister.setOnClickListener(v->{
+        binding.buttonConfirmRegister.setOnClickListener(v->{
             returnToLoginScreen();
         });
     }
@@ -61,29 +71,31 @@ public class RegisterActivity extends AppCompatActivity {
         User u = getUser();
         Bundle bundle = new Bundle();
         bundle.putParcelable(KEY_USER,u);
+        /*
         setResult(Activity.RESULT_OK,new Intent().putExtras(bundle));
         finish();
+         */
     }
 
     private User getUser() {
         User user = new User();
-        user.setName(registerBinding.nameTextRegister.getText().toString());
-        user.setLastName(registerBinding.lastNameTextRegister.getText().toString());
-        user.setPassword(registerBinding.lastNameTextRegister.getText().toString());
-        user.setAgeRange(registerBinding.agesRangeListRegister.getText().toString());
+        user.setName(binding.nameTextRegister.getText().toString());
+        user.setLastName(binding.lastNameTextRegister.getText().toString());
+        user.setPassword(binding.lastNameTextRegister.getText().toString());
+        user.setAgeRange(binding.agesRangeListRegister.getText().toString());
         return user;
     }
 
     private void launchCamera() {
-        AskPermissions.askPermissionCamera(this);
-        if(AskPermissions.isPermissionCameraOn(this))
-            resultCamera.launch(Intents.openCamera(this));
+        AskPermissions.askPermissionCamera(getActivity());
+        if(AskPermissions.isPermissionCameraOn(getActivity()))
+            resultCamera.launch(Intents.openCamera(getActivity()));
         else
-            Toast.makeText(this, getString(R.string.error_permission_camera), Toast.LENGTH_SHORT).show();
+            Toast.makeText(requireContext(), getString(R.string.error_permission_camera), Toast.LENGTH_SHORT).show();
     }
 
     private void inputListener() {
-        registerBinding.nameTextRegister.addTextChangedListener(new TextWatcher() {
+        binding.nameTextRegister.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -97,14 +109,14 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 if(!UtilsStrings.rergularExpressions(s.toString(), UserRegex.REGEX_NAME))
-                    registerBinding.nameRegister.setError(getString(R.string.error_name_register));
+                    binding.nameRegister.setError(getString(R.string.error_name_register));
                 else
-                    registerBinding.nameRegister.setError(null);
+                    binding.nameRegister.setError(null);
                 isAllFineToConfirm();
             }
         });
 
-        registerBinding.lastNameTextRegister.addTextChangedListener(new TextWatcher() {
+        binding.lastNameTextRegister.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -118,13 +130,13 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 if(!UtilsStrings.rergularExpressions(s.toString(), UserRegex.REGEX_LAST_NAME))
-                    registerBinding.lastNameRegister.setError(getString(R.string.error_last_name_register));
+                    binding.lastNameRegister.setError(getString(R.string.error_last_name_register));
                 else
-                    registerBinding.lastNameRegister.setError(null);
+                    binding.lastNameRegister.setError(null);
                 isAllFineToConfirm();
             }
         });
-        registerBinding.agesRangeListRegister.addTextChangedListener(new TextWatcher() {
+        binding.agesRangeListRegister.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -138,33 +150,33 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 if (arrayAges.indexOf(s.toString()) < arrayAges.indexOf(getResources().getString(R.string.age_range_adult)) && !s.toString().isEmpty())
-                    registerBinding.agesRangeRegister.setError(getResources().getText(R.string.error_agesRange_lower_years));
+                    binding.agesRangeRegister.setError(getResources().getText(R.string.error_agesRange_lower_years));
 
                 else
-                    registerBinding.agesRangeRegister.setError(null);
+                    binding.agesRangeRegister.setError(null);
                 isAllFineToConfirm();
             }
         });
     }
 
     private void isAllFineToConfirm() {
-        boolean correctName = registerBinding.nameRegister.getError() == null
-                && !registerBinding.nameTextRegister.getText().toString().isEmpty();
+        boolean correctName = binding.nameRegister.getError() == null
+                && !binding.nameTextRegister.getText().toString().isEmpty();
 
-        boolean correctLastName = registerBinding.lastNameRegister.getError() == null
-                && !registerBinding.lastNameTextRegister.getText().toString().isEmpty();
+        boolean correctLastName = binding.lastNameRegister.getError() == null
+                && !binding.lastNameTextRegister.getText().toString().isEmpty();
 
-        boolean correctAgeRange = registerBinding.agesRangeRegister.getError() == null
-                && !registerBinding.agesRangeListRegister.getText().toString().isEmpty();
+        boolean correctAgeRange = binding.agesRangeRegister.getError() == null
+                && !binding.agesRangeListRegister.getText().toString().isEmpty();
 
         if(correctName && correctLastName    && correctAgeRange)
-            registerBinding.buttonConfirmRegister.setEnabled(true);
+            binding.buttonConfirmRegister.setEnabled(true);
         else
-            registerBinding.buttonConfirmRegister.setEnabled(false);
+            binding.buttonConfirmRegister.setEnabled(false);
 
     }
     private void setValueSpinnerAges() {
-        adapterSpinnerAges =  ArrayAdapter.createFromResource(this,R.array.age_range,android.R.layout.simple_spinner_dropdown_item);
-        registerBinding.agesRangeListRegister.setAdapter(adapterSpinnerAges);
+        adapterSpinnerAges =  ArrayAdapter.createFromResource(requireContext(),R.array.age_range,android.R.layout.simple_spinner_dropdown_item);
+        binding.agesRangeListRegister.setAdapter(adapterSpinnerAges);
     }
 }
